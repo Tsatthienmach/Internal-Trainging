832addfa8a8e9e189e71704e0bdd26ba7ae469d9
Tranh Dong <dongquoctranh@gmail.com>
2021-03-15 04:19:44 +0700

    database sqlalchemy

---
+++ Database/mysql.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql+pymysql://cookiemonster:chocolatechip' \
+                       '@mysql01.monster.internal/cookies', pool_recycle=3600)
+connection = engine.connect()
